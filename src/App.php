5c17a8f3374aae615dc48767d5e102fa04d67772
Vali <valibojici@gmail.com>
2022-01-30 16:02:31 +0200

    added HomeController

--- src/App.php
+++ src/App.php
@@ -4,23 +4,25 @@ namespace App;
 
 use App\Router;
 use App\Request;
+use App\Controllers\HomeController;
+
 
 class App{
     private $router;
     private $request;
-    public function __construct(){
+    
+    public function __construct()
+    {
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->router = new Router();
 
-        $this->router->get('/scandiweb', function() {
-            echo 'root';
-        });
+        $this->router->get('/scandiweb', [new HomeController(), 'defaultAction']);
 
         $this->router->get('/scandiweb/add-product', function($params) {
             echo 'caca </br>';
             var_dump($params);
         });
 
-        $this->router->run();
+        $this->router->run($this->request);
     }
 }
